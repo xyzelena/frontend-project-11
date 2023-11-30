18214ec51a0b5b770f761bcc4b3651c9a7de5767
Elena <emkhuzhina@gmail.com>
2023-12-01 01:18:05 +0530

    add onChange

--- src/js/application.js
+++ src/js/application.js
@@ -1,7 +1,6 @@
 import * as yup from 'yup';
-import onChange from 'on-change';
-import _ from 'lodash';
-
+import onChangeState from './View.js';
+//import _ from 'lodash';
 
 const app = () => {
     const elements = {
@@ -16,87 +15,42 @@ const app = () => {
 
     const initialState = {
         rssForm: {
-            status: 'idle', // по умолчанию простой 
+            valid: true,
             fields: {
                 url: '',
             },
-            errors: {},
+            error: '',
             feeds: [],
         },
     };
 
-    const watchedState = onChange(initialState, (path, value) => {
-        if (path === 'rssForm.status') {
-            switch (value) {
-                case 'invalid': {
-                    // Отрисовка ошибок, хранящихся в состоянии errors
-                    const errors = watchedState.rssForm.errors;
-                    console.log(watchedState.rssForm.errors); 
-
-                    input.classList.add('is-invalid');
-
-                    const nextEl = input.nextElementSibling;
-
-                    // если элемент является последним в своём род узле
-                    if (nextEl === null) {
-                        const errorDiv = document.createElement('div');
-                        errorDiv.classList.add('invalid-feedback');
-                        errorDiv.innerHTML = errors;
-                        input.after(errorDiv);
-                    }
-
-                    break;
-                }
-
-                case 'valid': {
-                    //удалить ошибки 
-                    elements.fields.input.classList.remove('is-invalid');
-
-                    document.querySelectorAll('invalid-feedback')
-                        .forEach(elem => elem.remove());
-
-                    break;
-                }
-
-                // case 'failed': { // проблема с сетью 
-                //     break; 
-                //   }
-
-                // case 'finished': {
-                //     break; 
-                //   }
-
-                default:
-                    throw new Error(`Error in ${value}!`);
-            }//end switch 
-        }//end if path 
-    });//end watchedState 
-
-    const schema = yup.object().shape({
-        url: yup.string()
-            .url('url must be correct')
-            .required('url must be required')
-            .notOneOf(watchedState.rssForm.feeds, 'url must be uniq'),
-    });
-
+    const watchedState = onChangeState(initialState,input);
 
     elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        watchedState.rssForm.fields.url = elements.fields.input.value;
+        const url = input.value.trim(); 
+
+        watchedState.rssForm.fields.url = url;
+
+        const feeds = Object.values(watchedState.rssForm.feeds);
+
+        const schema = yup.object().shape({
+            url: yup.string()
+                .url('url must be correct')
+                .required('url must be required')
+                .notOneOf(feeds, 'url must be uniq'),
+        });
 
         const validateStatus = schema
             .validate(watchedState.rssForm.fields, { abortEarly: false })
             .then(() => {
-                watchedState.rssForm.status = 'valid';
-                watchedState.rssForm.feeds.push(elements.fields.input.value); 
-                console.log(watchedState.rssForm.feeds); 
+                watchedState.rssForm.feeds.push(url);
+                watchedState.rssForm.valid = true;
             })
             .catch((error) => {
-                //console.log('[error]', error);
-                watchedState.rssForm.status = 'invalid';
-                watchedState.rssForm.errors = error.errors[0];
-                console.log(watchedState.rssForm.errors); 
+                watchedState.rssForm.error = error.message;
+                watchedState.rssForm.valid = false;
             });
 
     });//end addEventListener
