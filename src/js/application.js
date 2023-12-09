71c5ec27844fb876c246a93dd771b60378fd0637
Elena <emkhuzhina@gmail.com>
2023-12-10 01:41:52 +0530

    add i18next

--- src/js/application.js
+++ src/js/application.js
@@ -1,8 +1,23 @@
 import * as yup from 'yup';
+import i18next from 'i18next';
+import resources from '../locales/index.js';
 import onChangeState from './View.js';
 //import _ from 'lodash';
 
 const app = () => {
+
+    const defaultLanguage = 'ru';
+
+    const i18nInstance = i18next.createInstance();
+
+    i18nInstance
+        .init({
+            lng: defaultLanguage,
+            debug: false,
+            resources,
+        })
+        .then(function (t) { t('key'); });
+
     const elements = {
         form: document.querySelector('.rss-form'),
         fields: {
@@ -24,22 +39,35 @@ const app = () => {
         },
     };
 
-    const watchedState = onChangeState(initialState,input);
+    const watchedState = onChangeState(initialState, input);
 
     elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        const url = input.value.trim(); 
+        const url = input.value.trim();
 
         watchedState.rssForm.fields.url = url;
 
         const feeds = Object.values(watchedState.rssForm.feeds);
 
+        yup.setLocale({
+            // use constant translation keys for messages without values
+            mixed: {
+                url: 'urlNotCorrect',
+                required: 'urlRequired',
+                notOneOf: 'urlNotOneOf',
+                default: 'fieldInvalid',
+            },
+        });
+
         const schema = yup.object().shape({
             url: yup.string()
-                .url('url must be correct')
-                .required('url must be required')
-                .notOneOf(feeds, 'url must be uniq'),
+                // .url('url must be correct')
+                // .required('url must be required')
+                // .notOneOf(feeds, 'url must be uniq'),
+                .url()
+                .required()
+                .notOneOf(feeds, ''),
         });
 
         const validateStatus = schema
@@ -48,8 +76,10 @@ const app = () => {
                 watchedState.rssForm.feeds.push(url);
                 watchedState.rssForm.valid = true;
             })
-            .catch((error) => {
-                watchedState.rssForm.error = error.message;
+            .catch((err) => {
+                //watchedState.rssForm.error = error.message;
+
+                watchedState.rssForm.error = err.errors.map((err) => i18nInstance.t(`mistakes.${err.key}`));
                 watchedState.rssForm.valid = false;
             });
 
