6672e7a88dfba46f6e271ae2723058c7f5cf4776
Elena <emkhuzhina@gmail.com>
2023-12-10 01:42:43 +0530

    add translations for i18next

---
+++ src/locales/ru.js
@@ -0,0 +1,10 @@
+export default {
+    translation: {
+        mistakes: {
+            urlNotCorrect: 'Введен некорректный адрес',
+            urlRequired: 'Необходимо указать адрес',
+            urlNotOneOf: 'Необходимо указать новый адрес',
+            fieldInvalid: 'Поле заполнено некорректно',
+        }
+    },
+};
