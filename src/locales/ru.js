0c382a36048c70bc371599441ded38fe18fae31d
Elena <emkhuzhina@gmail.com>
2024-02-14 00:16:16 +0530

    add baseTextUI

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,5 +1,16 @@
 export default {
     translation: {
+        baseTextUI: {
+            header: 'RSS агрегатор',
+            leadText: 'Начните читать RSS сегодня! Это легко, это красиво.',
+            placeholderInput: 'ссылка RSS',
+            labelInput: 'Ссылка RSS',
+            btnSubmit: 'Добавить',
+            exampleUrl: 'Пример: https://ru.hexlet.io/lessons.rss',
+            footerText: 'Ссылка на проект:',
+            footerLink: 'GitHub',
+        },
+
         mistakes: {
             url: 'Ссылка должна быть валидным URL',
             required: 'Не должно быть пустым',
