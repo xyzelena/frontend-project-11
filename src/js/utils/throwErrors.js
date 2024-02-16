b0b2a19484dbc6ae2fc5c600463c61600da29594
Elena <emkhuzhina@gmail.com>
2024-02-17 00:40:30 +0530

    change func statusResponse 200

--- src/js/utils/throwErrors.js
+++ src/js/utils/throwErrors.js
@@ -1,11 +1,9 @@
 export const throwErrorResponse = (statusResponse) => {
-    if (statusResponse === 304) {
-        throw new Error('networkError');
-    }
-    else if (statusResponse >= 400 && statusResponse < 500) {
+    if (statusResponse >= 200 && statusResponse < 300) {
+        return;
+    } else if (statusResponse >= 400 && statusResponse < 500) {
         throw new Error('noDataContents');
-    } else if (statusResponse >= 500 && statusResponse < 600) {
+    } else {
         throw new Error('networkError');
     }
-    else return;
 };
