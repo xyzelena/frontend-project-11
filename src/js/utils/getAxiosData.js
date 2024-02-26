d2d5e9eaf890670e7801595554e19c20d0dd21bb
Elena <emkhuzhina@gmail.com>
2024-02-27 01:36:15 +0530

    add catch with Error

--- src/js/utils/getAxiosData.js
+++ src/js/utils/getAxiosData.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const getAxiosData = (url) => axios.get(url);
+const getAxiosData = (url) => {
+    return axios.get(url)
+        .catch((error) => {
+            throw new Error('networkError');
+        });
+}
 
 export default getAxiosData; 
